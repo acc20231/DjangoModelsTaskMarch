eebca20a12838cf0210b96820d4369a1e1dc9a32
marc <alexander-9090@inbox.ru>
2024-04-28 17:27:31 +0400

    /

--- SiteDog/users/forms.py
+++ SiteDog/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class LoginUserForm(forms.Form):
-    usernames = forms.CharField(label="Логин",
+    username = forms.CharField(label="Логин",
                                widget=forms.TimeInput(attrs={'class': 'form-input'}))
-    passwordd = forms.CharField(label="Пароль",
+    password = forms.CharField(label="Пароль",
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
