e9037ca79661e17acb367e8a94cbef85bec622ee
marc <alexander-9090@inbox.ru>
2024-04-30 17:26:09 +0400

    /

--- SiteDog/Dog/models.py
+++ SiteDog/Dog/models.py
@@ -17,6 +17,7 @@ class Dog(models.Model):
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager()
     published = PublishedManager()
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -31,4 +32,9 @@ class Dog(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
