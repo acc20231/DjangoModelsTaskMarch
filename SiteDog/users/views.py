22eff302b4f5424dc16f60be24e0ebf1bd5b6c3e
marc <alexander-9090@inbox.ru>
2024-05-05 01:44:59 +0400

    Регистрация пользователей через функции представления. Пароли представлены в READ.me.Класс UserCreationForm

--- SiteDog/users/views.py
+++ SiteDog/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import LoginUserForm, RegisterUserForm
 
@@ -8,14 +10,20 @@ class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegisterUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data['password'])
+#             user.save()
+#             return render(request, 'users/register_done.html')
+#     form = RegisterUserForm()
+#     return render(request, 'users/register.html', {'form': form})
