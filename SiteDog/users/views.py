11ebf857727983d9950d81688df2e32f88eab132
marc <alexander-9090@inbox.ru>
2024-05-05 15:50:27 +0400

    Авторизация через email. Профайл пользователя. Реализация алгоритма смены пароля пользователя через классы PasswordChangeView и PasswordChangeDoneView

--- SiteDog/users/views.py
+++ SiteDog/users/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from .forms import LoginUserForm, RegisterUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm, UserPasswordChangeForm
 
 
 class LoginUser(LoginView):
@@ -17,13 +19,19 @@ class RegisterUser(CreateView):
     success_url = reverse_lazy('users:login')
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         form = RegisterUserForm(request.POST)
-#         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.set_password(form.cleaned_data['password'])
-#             user.save()
-#             return render(request, 'users/register_done.html')
-#     form = RegisterUserForm()
-#     return render(request, 'users/register.html', {'form': form})
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': 'Профиль пользователя'}
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
